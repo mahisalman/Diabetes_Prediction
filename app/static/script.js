40ccc3836f6d09d064de11314c319c2909efeb82
mahisalman <mahi.salman@gmail.com>
2025-08-18 12:24:56 +0600

    updated

--- app/static/script.js
+++ app/static/script.js
@@ -8,7 +8,7 @@ document.getElementById("form").addEventListener("submit", async function (e) {
     data[key] = Number(value);
   });
 
-  const response = await fetch("http://127.0.0.1:9000/predict", {
+  const response = await fetch("https://diabetes-prediction-6z0g.onrender.com/predict", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
